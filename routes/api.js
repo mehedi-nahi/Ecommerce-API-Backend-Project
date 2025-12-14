4105fb67bd0adc0f76b1798b1129c06b2fb757d3
Nahi <nahimehedi@gmail.com>
2025-12-14 20:28:58 +0600

    Initial commit: Ecommerce API setup

--- routes/api.js
+++ routes/api.js
@@ -1,29 +1,12 @@
 import express from "express";
 const router = express.Router();
-
-import AuthMiddleware from "../app/middlewares/AuthMiddleware.js";
-
-import * as TaskController from "../app/controllers/TaskController.js";
 import * as UserController from "../app/controllers/UserController.js";
 
 
 
 //Users
-router.post("/Registration", UserController.Registration)
+
 router.post("/Login", UserController.Login)
-router.get("/ProfileDetails", AuthMiddleware,UserController.ProfileDetails)
-router.post("/ProfileUpdate", AuthMiddleware, UserController.ProfileUpdate)
-router.get("/EmailVerify/:email",UserController.EmailVerify)
-router.post("/CodeVerify",UserController.CodeVerify)
-router.post("/ResetPassword",UserController.ResetPassword)
 
-
-//Task
-
-router.post("/CreateTask", TaskController.CreateTask)
-router.get("/UpdateTaskStatus",TaskController.UpdateTaskStatus)
-router.get("/TaskListByStatus",TaskController.TaskListByStatus)
-router.get("/DeleteTask",TaskController.DeleteTask)
-router.get("/CountTask",TaskController.CountTask)
 
 export default router;
