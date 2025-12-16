73f6e356a8339ae8839068632792598631bbf3e7
Nahi <nahimehedi@gmail.com>
2025-12-17 04:09:10 +0600

    CartService Completed

--- app/controllers/CartListController.js
+++ app/controllers/CartListController.js
@@ -1,33 +1,28 @@
+import {CreateCartService,
+    RemoveCartService,
+    ReadCartListService,
+    UpdateCartService
+} from "../service/CartService.js";
+
 export const CreateCart=async(req,res)=>{
-    try{
-        return res.json({status:"success","Message": "CreateCart successfully"})
-    }
-    catch(e){
-        return res.json({status:"error","Message": e.toString()});
-    }
+    let result = await CreateCartService(req)
+    return res.json(result)
 }
 
+
 export const UpdateCart=async(req,res)=>{
-    try{
-        return res.json({status:"success","Message": "UpdateCart successfully"})
-    }
-    catch(e){
-        return res.json({status:"error","Message": e.toString()});
-    }
+    let result = await UpdateCartService(req)
+    return res.json(result)
 }
+
+
 export const ReadCartList=async(req,res)=>{
-    try{
-        return res.json({status:"success","Message": "ReadCartList successfully"})
-    }
-    catch(e){
-        return res.json({status:"error","Message": e.toString()});
-    }
+    let result = await ReadCartListService(req)
+    return res.json(result)
 }
+
+
 export const RemoveCart=async(req,res)=>{
-    try{
-        return res.json({status:"success","Message": "RemoveCart successfully"})
-    }
-    catch(e){
-        return res.json({status:"error","Message": e.toString()});
-    }
+    let result = await RemoveCartService(req)
+    return res.json(result)
 }
